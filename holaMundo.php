5e6a0f6013d5a5f10f6b6ce0c2d28079de7bfe04
TheEduPro255 <73311071+TheEduPro255@users.noreply.github.com>
2023-09-20 10:07:05 +0200

    Add files via upload

---
+++ holaMundo.php
@@ -0,0 +1,17 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Hola Mundo";
+    ?>
+    <p></p>
+    <a href="https://github.com/TheEduPro255/DWES/blob/main/holaMundo.php">Ver codigo</a>
+</body>
+</html>
