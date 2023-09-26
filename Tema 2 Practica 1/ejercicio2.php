eb06db3f2ab8954d1441aa2fbba7568b3f2cc66a
TheEduPro255 <73311071+TheEduPro255@users.noreply.github.com>
2023-09-26 08:25:18 +0200

    Update ejercicio2.php

--- Tema 2 Practica 1/ejercicio2.php
+++ Tema 2 Practica 1/ejercicio2.php
@@ -1,4 +1,11 @@
 <?php
+/** 
+ *Script del portfolio
+
+ *@autor: Eduardo Ruz
+ *@Version 0.01a
+*/
+
 echo "<img src = '../foto.png'/><br>";
 echo "<a href = 'https://github.com/TheEduPro255/DWES/blob/main/ejercicio2.php'>Ver codigo</a>"; 
 ?>
