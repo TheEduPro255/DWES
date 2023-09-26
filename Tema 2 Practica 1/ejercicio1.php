ae29d407fcbbe14d6401d2bef7961c9084365fb0
TheEduPro255 <73311071+TheEduPro255@users.noreply.github.com>
2023-09-26 08:24:19 +0200

    Update ejercicio1.php

--- Tema 2 Practica 1/ejercicio1.php
+++ Tema 2 Practica 1/ejercicio1.php
@@ -1,4 +1,11 @@
 <?php
+/** 
+ *Script del portfolio
+
+ *@autor: Eduardo Ruz
+ *@Version 0.01a
+*/
+
 echo "'Hola Mundo <br>'";
 echo "<a href = 'https://github.com/TheEduPro255/DWES/blob/main/ejercicio1.php'>Ver codigo</a>";
 ?>
