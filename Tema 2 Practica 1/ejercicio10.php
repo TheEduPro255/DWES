327bec58f0945c6621b0eebb8cf761d298dd883d
TheEduPro255 <73311071+TheEduPro255@users.noreply.github.com>
2023-09-26 09:23:27 +0200

    Add files via upload

---
+++ Tema 2 Practica 1/ejercicio10.php
@@ -0,0 +1,24 @@
+<?php
+$nombre = "Eduardo";
+$edad = 25;
+
+$miInformacion = <<<EOD
+Nombre: $nombre <br>
+Edad: $edad años <br>
+EOD;
+
+echo $miInformacion;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<a href = "https://github.com/TheEduPro255/DWES/blob/main/Tema%202%20Practica%201/ejercicio9.php">Ver codigo</a>
+</body>
+</html>
