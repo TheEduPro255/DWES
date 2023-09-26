e3c236aca70613122f2b5f8df10b7c9d29263b38
TheEduPro255 <73311071+TheEduPro255@users.noreply.github.com>
2023-09-26 08:28:34 +0200

    Update ejercicio7.php

--- Tema 2 Practica 1/ejercicio7.php
+++ Tema 2 Practica 1/ejercicio7.php
@@ -1,4 +1,12 @@
 <?php
+/** 
+ *Script del portfolio
+
+ *@autor: Eduardo Ruz
+ *@Version 0.01a
+*/
+
+
 $a = 8;
 echo "Valor actual ".$a."<br>";
 $a += 2;
@@ -14,6 +22,7 @@ echo "Incrementa el valor en 1. Valor ahora ".$a."<br>";
 $a -= 1;
 echo "Decrementa el valor en 1. Valor ahora ".$a."<br>"; 
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
